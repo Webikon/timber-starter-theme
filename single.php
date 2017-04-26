83bf8a755eac12e755dfb7cef35b05a3265e371d
Pavol Caban <pavol.caban@webikon.sk>
2017-04-26 10:17:13 +0200

    Better views structure, fix internationalization, adjust classes

--- single.php
+++ single.php
@@ -14,8 +14,4 @@ $post = Timber::query_post();
 $context['post'] = $post;
 $context['comment_form'] = TimberHelper::get_comment_form();
 
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
-} else {
-	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
-}
+Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
