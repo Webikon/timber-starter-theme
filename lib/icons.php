23908bf920a09699feb26e056f28d945b24ba1d0
Pavol Caban <pavol.caban@webikon.sk>
2017-06-11 18:28:53 +0200

    Fix error when no icons directory found

--- lib/icons.php
+++ lib/icons.php
@@ -8,54 +8,57 @@
  */
 
 
- /**
-  * Wrapper function to get svg icon from a file.
-  * Returns inline svg markup.
-  *
-  * @param type|string $name
-  * @return html
-  */
- function wbkn_get_icon( $name = '' ) {
- 	$file_location = locate_template( 'dist/images/icons/icon-' . $name . '.svg' );
+/**
+* Wrapper function to get svg icon from a file.
+* Returns inline svg markup.
+*
+* @param type|string $name
+* @return html
+*/
+function wbkn_get_icon( $name = '' ) {
+	$file_location = locate_template( 'dist/images/icons/icon-' . $name . '.svg' );
 
- 	if ( ! $file_location ) {
- 		$file_location = locate_template( 'assets/images/icons/icon-' . $name . '.svg' ); // fallback for old theme
- 	}
+	if ( ! $file_location ) {
+		$file_location = locate_template( 'assets/images/icons/icon-' . $name . '.svg' ); // fallback for old theme
+	}
 
- 	if ( $file_location ) {
- 		return '<span class="icon icon-' . $name . '">' . file_get_contents( $file_location ) . '</span>';
- 	} else {
- 		var_dump( 'Icon not found' );
- 	}
- }
+	if ( $file_location ) {
+		return '<span class="icon icon-' . $name . '">' . file_get_contents( $file_location ) . '</span>';
+	} else {
+		var_dump( 'Icon not found' );
+	}
+}
 
- /**
-  * Get all svg icons from theme icons folder.
-  *
-  * @return array $icons
-  */
- function wbkn_get_all_icons() {
- 	$icons = array();
- 	$icons_dir = get_stylesheet_directory() . '/dist/images/icons';
+/**
+* Get all svg icons from theme icons folder.
+*
+* @return array $icons
+*/
+function wbkn_get_all_icons() {
+	$icons = array();
+	$icons_dir = get_stylesheet_directory() . '/dist/images/icons';
 
- 	// Read icons folder and every file in it
- 	foreach( scandir( $icons_dir ) as $filename ) {
- 		// Full path to current icon
- 		$path = $icons_dir . '/' . $filename;
+	if ( ! file_exists( $icons_dir ) ) {
+		return;
+	}
+	// Read icons folder and every file in it
+	foreach( scandir( $icons_dir ) as $filename ) {
+		// Full path to current icon
+		$path = $icons_dir . '/' . $filename;
 
- 		if ( is_file( $path ) ) {
- 			$file_parts = pathinfo( $path );
+		if ( is_file( $path ) ) {
+			$file_parts = pathinfo( $path );
 
- 			// Check for proper file extension
- 			if ( $file_parts['extension'] == 'svg' ) {
- 				// Remove .svg extension so we get icon name
- 				$name = str_replace( '.svg', '', $filename );
+			// Check for proper file extension
+			if ( $file_parts['extension'] == 'svg' ) {
+				// Remove .svg extension so we get icon name
+				$name = str_replace( '.svg', '', $filename );
 
- 				// Get icon's file content and save it to array
- 				$icons[$name] = '<span class="icon icon-' . $name . '">' . file_get_contents( $path  ) . '</span>';
- 			}
- 		}
- 	}
+				// Get icon's file content and save it to array
+				$icons[$name] = '<span class="icon icon-' . $name . '">' . file_get_contents( $path  ) . '</span>';
+			}
+		}
+	}
 
- 	return $icons;
- }
+	return $icons;
+}
