70879859b2766980492b3227aa7561d96b96ead1
Pavol Caban <pavol.caban@webikon.sk>
2017-08-14 19:15:48 +0200

    Rework css classes to BEM, add Foundation Toggler support (e.g. for responsive menu)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,9 @@
 ( function($) {
 	$(document).foundation();
 
+	// Add additional 'is-expanded' class to the button which triggers Foundation Toggle
+	$('[data-toggle]').on( 'click', function() {
+		$(this).toggleClass('is-expanded');
+	})
+
 })(jQuery);
